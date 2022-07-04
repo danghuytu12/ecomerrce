8a182c14e98f6b17d33f5dd7f7ebf82387b4fed0
Huy Tú <danghuytu.thd2006@gmail.com>
2022-07-05 00:00:33 +0700

    create categories

--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BtnRender from "./BtnRender";
-function ProductItem({ product, isAdmin ,handleCheck}) {
-   console.log('productproduct',product) 
+function ProductItem({ product, isAdmin, handleCheck }) {
+  console.log("productproduct", product);
   return (
     <div className="product_card">
-      {isAdmin && <input type="checkbox" checked={product.checked} onChange={() => handleCheck(product._id)}/>}
-      <img src={product.images.url? product.images.url:product.images[0].url} alt="" />
+      {isAdmin && (
+        <input
+          type="checkbox"
+          checked={product.checked}
+          onChange={() => handleCheck(product._id)}
+        />
+      )}
+      <img
+        src={product.images.url ? product.images.url : product.images[0].url}
+        alt=""
+      />
 
       <div className="product_box">
         <h2 title={product.title}>{product.title}</h2>
